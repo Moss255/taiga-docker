7fa69cba830c335975fa14a0e68ec1d4d921a25e
Jack Moss <jackhmoss@gmail.com>
2025-10-26 11:56:13 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -1,32 +1,9 @@
 import os
 from .common import *
 
-#######################################################################
-
-# Read database settings from environment variables
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
-
-#######################################################################
-
-# Define the database connection, adding the required SSL mode
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": POSTGRES_DB,
-        "USER": POSTGRES_USER,
-        "PASSWORD": POSTGRES_PASSWORD,
-        "HOST": POSTGRES_HOST,
-        "PORT": POSTGRES_PORT,
-        "OPTIONS": {
-            "sslmode": "require",
-        },
-    }
+# Override the DATABASES dictionary to add the required SSL mode.
+# The other database settings (USER, PASSWORD, HOST, etc.) are already
+# loaded from environment variables by the default `common.py` settings.
+DATABASES["default"]["OPTIONS"] = {
+    "sslmode": "require"
 }
-
-#######################################################################
-
-# You can leave the rest of the file empty unless you need other advanced settings.
