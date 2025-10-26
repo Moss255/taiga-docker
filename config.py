2adfd6af2a9898a0631a4190e8c09ee5d532238e
Jack Moss <jackhmoss@gmail.com>
2025-10-26 12:11:58 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -11,3 +11,15 @@ DATABASES["default"]["HOST"] = os.getenv("POSTGRES_HOST")
 DATABASES["default"]["USER"] = os.getenv("POSTGRES_USER")
 DATABASES["default"]["PASSWORD"] = os.getenv("POSTGRES_PASSWORD")
 DATABASES["default"]["OPTIONS"] = { "sslmode": "require" }
+
+#######################################################################
+
+# Celery/RabbitMQ settings
+#
+BROKER_URL = "amqp://{user}:{password}@{hostname}:{port}/{vhost}".format(
+    user=os.getenv("RABBITMQ_USER", "taiga"),
+    password=os.getenv("RABBITMQ_PASS", "taiga"),
+    hostname=os.getenv("RABBITMQ_HOST", "taiga-rabbitmq"),
+    port=os.getenv("RABBITMQ_PORT", 5672),
+    vhost=os.getenv("RABBITMQ_VHOST", "taiga"),
+)
