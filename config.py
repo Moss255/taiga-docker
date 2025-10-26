f6747efc78ba0da918e7d8439ed2566ad89cedb3
Jack Moss <jackhmoss@gmail.com>
2025-10-26 09:45:28 +0300

    Amended ssl issue with postgres db

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+# Read database settings from environment variables
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
+
+# Define the database connection, adding the required SSL mode
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": POSTGRES_DB,
+        "USER": POSTGRES_USER,
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": POSTGRES_HOST,
+        "PORT": POSTGRES_PORT,
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    }
+}
+
+# You can leave the rest of the file empty unless you need other advanced settings.
